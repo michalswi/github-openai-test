5f12f00872d459a1ba00438277988725ac2d6d95
ms <>
2026-03-06 13:39:46 +0100

    main

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	var port = "5050"
+	http.HandleFunc("/", home)
+	log.Println("Server is ready to handle requests at port", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	log.Println("User-Agent:", r.UserAgent())
+	w.Header().Set("Content-Type", "application/text")
+	w.WriteHeader(http.StatusOK)
+	response := "ok"
+	_, err := w.Write([]byte(response))
+	if err != nil {
+		log.Println("Error writing response:", err)
+	}
+}
